7a1fd43934695b64371fb107638152eee586e89b
mmdcloud <madmaxcloudpractice@gmail.com>
2025-06-06 08:51:18 +0530

    update: updated frontend  and backend folders

--- backend/api/artifact_push.sh
+++ backend/api/artifact_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir code
-cp -r ../backend/api/* code/
-cd code
+mkdir backend-code
+cp -r ../../../backend/api/* backend-code/
+cd backend-code
 
 docker buildx build --tag carshub-backend --file ./Dockerfile .
-docker tag carshub-backend:latest us-central1-docker.pkg.dev/carshub-447206/carshub-backend/carshub-backend:latest
-docker push us-central1-docker.pkg.dev/carshub-447206/carshub-backend/carshub-backend:latest
+docker tag carshub-backend:latest us-central1-docker.pkg.dev/$1/carshub-backend/carshub-backend:latest
+docker push us-central1-docker.pkg.dev/$1/carshub-backend/carshub-backend:latest
--- backend/cloud-run-function/main.py
+++ backend/cloud-run-function/main.py
@@ -1,29 +1,44 @@
 import os
 import mysql.connector
 import sqlalchemy
+import json
+from google.cloud import storage
+import base64
 from datetime import datetime
 from google.cloud import secretmanager
 from google.cloud import functions_v1
 from google.auth import default
 
-# Cloud SQL connection settings
-INSTANCE_CONNECTION_NAME = os.environ.get('INSTANCE_CONNECTION_NAME')
-# 'your-project-id:your-region:your-instance-id'
 DB_USER = os.environ.get('DB_USER')
 DB_PATH = os.environ.get('DB_PATH')
 DB_NAME = "carshub"
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
+SECRET_NAME = os.environ.get('SECRET_NAME')
 table_name = "InventoryImages"
+
+storage_client = storage.Client()
+
+def access_secret(secret_name, version_id="1"):
+    # Create the Secret Manager client
+    client = secretmanager.SecretManagerServiceClient()
+    
+    # Build the resource name of the secret version
+    name = f"{secret_name}/versions/{version_id}"
+    
+    # Access the secret version
+    response = client.access_secret_version(request={"name": name})
+    
+    # Return the decoded payload
+    return response.payload.data.decode("UTF-8")
 
 # Function to connect to Cloud SQL
 def get_db_connection():
+    db_password = access_secret(SECRET_NAME)
     """Return a MySQL database connection."""
     connection = mysql.connector.connect(
         user=DB_USER,
         database=DB_NAME,
-        # host=DB_PATH,
-        unix_socket=f'/cloudsql/{INSTANCE_CONNECTION_NAME}',
-        password=DB_PASSWORD
+        host=DB_PATH,        
+        password=db_password
     )
     return connection
 
@@ -31,7 +46,7 @@ def insertRecord(connection,inventoryId,path,type,description):
     cursor = connection.cursor()
     ts = datetime.now().isoformat()
     insert_query = "INSERT INTO InventoryImages (inventoryId,path,type,description,createdAt,updatedAt) VALUES (%s, %s, %s, %s, %s, %s)"
-    cursor.execute(insert_query, (1,path,type,description,ts,ts))
+    cursor.execute(insert_query, (inventoryId,path,type,description,ts,ts))
     connection.commit()
 
 def handler(event, context):
@@ -41,8 +56,13 @@ def handler(event, context):
     print("Context")
     print(context)
     try:
-        connection = get_db_connection()        
-        insertRecord(connection,event["metadata"]["inventoryid"], "b", event["metadata"]["typeofdocument"], event["metadata"]["descriptionofdocument"])
+        connection = get_db_connection()
+        if event["attributes"]["eventType"] == "OBJECT_FINALIZE":
+            decoded_dict = base64.b64decode(event["data"]).decode('utf-8')
+            final_dict = json.loads(decoded_dict)
+            bucket = storage_client.bucket(event["attributes"]["bucketId"])
+            blob = bucket.get_blob(final_dict["name"])
+            insertRecord(connection,blob.metadata["inventoryid"], final_dict["name"], blob.metadata["typeofdocument"], blob.metadata["descriptionofdocument"])
         connection.close()
         return f"Record inserted successfully.", 200
 
